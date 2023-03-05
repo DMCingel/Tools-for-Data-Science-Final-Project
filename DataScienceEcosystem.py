2db7ede2d8b2ad1e432ea4e765d07fd02e72a6eb
DMCingel <126984650+DMCingel@users.noreply.github.com>
2023-03-04 21:22:14 -0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## Author:
+#     DMC
+
+# Hello, and welcome! In this notebook, we will be summarizing a variety of tools used in data science as well as the ecosystems they can be used in.
+
+# **Objectives:**
+#  -List common data science languages
+#  -List common libraries used in data science across languages
+#  -List open source data science tools
+#  -Execute basic python arithmetic in code cells
+#  -Share your created Jupyter Notebook to GitHub
+
+# Many code-based languages can be used in data science to serve a variety of needs and preferences. Some of the most common languages include:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Deeper than the common languages used, there are a variety of libraries specific to each language that data scientists fiind to be of great use, including:
+# 1. Pandas in Python
+# 2. Numpy in Python
+# 3. datasets in R
+# 4. base in R
+
+# |Data Science Tools|
+# |------------------|
+# |GitHub            |
+# |Jupyter           |
+# |R Studio          |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# In[ ]:
+
+
+
+
